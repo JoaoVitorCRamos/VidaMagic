91151578d0a9722b8bc3619bddd13f9fe133e306
Joao Vitor Costa <139905189+JoaoVitorCRamos@users.noreply.github.com>
2023-08-17 17:46:54 -0300

    corrigi erros e coloquei atualizaçoes

--- script.js
+++ script.js
@@ -1,19 +1,57 @@
-const player1Life = document.getElementById('player1-life');
-const player2Life = document.getElementById('player2-life');
+const player1Life = document.getElementById("player1-life");
+const player2Life = document.getElementById("player2-life");
 
-document.getElementById('player1-decrease').addEventListener('click', () => decreaseLife(player1Life));
-document.getElementById('player1-increase').addEventListener('click', () => increaseLife(player1Life));
-document.getElementById('player2-decrease').addEventListener('click', () => decreaseLife(player2Life));
-document.getElementById('player2-increase').addEventListener('click', () => increaseLife(player2Life));
+const resetLife = () => {
+    player1Life.textContent = "20";
+    player2Life.textContent = "20";
+    // Clear any previous winner alerts
+    clearWinnerAlert();
+};
 
-function decreaseLife(element) {
-    let currentLife = parseInt(element.textContent);
-    if (currentLife > 0) {
-        element.textContent = currentLife - 1;
+const clearWinnerAlert = () => {
+    // Close the alert if it's still open
+    if (document.querySelector(".alert-overlay")) {
+        document.body.removeChild(document.querySelector(".alert-overlay"));
     }
-}
+};
 
-function increaseLife(element) {
-    let currentLife = parseInt(element.textContent);
-    element.textContent = currentLife + 1;
-}
+const checkWinner = () => {
+    const life1 = parseInt(player1Life.textContent);
+    const life2 = parseInt(player2Life.textContent);
+
+    if (life1 <= 0) {
+        alert("Player 2 wins!");
+        resetLife();
+    } else if (life2 <= 0) {
+        alert("Player 1 wins!");
+        resetLife();
+    }
+};
+
+document.getElementById("player1-decrease").addEventListener("click", () => {
+    let currentLife = parseInt(player1Life.textContent);
+    if (currentLife > 0) {
+        player1Life.textContent = currentLife - 1;
+        checkWinner();
+    }
+});
+
+document.getElementById("player1-increase").addEventListener("click", () => {
+    player1Life.textContent = parseInt(player1Life.textContent) + 1;
+});
+
+document.getElementById("player2-decrease").addEventListener("click", () => {
+    let currentLife = parseInt(player2Life.textContent);
+    if (currentLife > 0) {
+        player2Life.textContent = currentLife - 1;
+        checkWinner();
+    }
+});
+
+document.getElementById("player2-increase").addEventListener("click", () => {
+    player2Life.textContent = parseInt(player2Life.textContent) + 1;
+});
+
+document.getElementById("reset-button").addEventListener("click", () => {
+    resetLife();
+});
